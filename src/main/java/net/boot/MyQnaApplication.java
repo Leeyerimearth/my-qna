227f56c8f94f80ef4f855694125f22693bba6a19
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2020-03-08 21:54:21 +0900

    initial commit

---
+++ src/main/java/net/boot/MyQnaApplication.java
@@ -0,0 +1,13 @@
+package net.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQnaApplication.class, args);
+	}
+
+}
